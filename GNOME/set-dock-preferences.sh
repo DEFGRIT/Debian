d533c9306fd79bcbbd5ebaa9eba32bbc1677794a
DETGRIT <dg@defgrit.nl>
2026-02-26 21:31:15 +0100

    Create set-dock-preferences.sh

---
+++ GNOME/set-dock-preferences.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'LEFT'
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size '32'
